06a7dbcada87ed72a20976d9dcda277b9bee700d
Your Name <you@example.com>
2019-06-25 16:18:42 +0530

    mongodb update

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 var db;
 
 // Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
+mongodb.MongoClient.connect(process.env.MONGODB_URI || " mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb", function (err, client) {
   if (err) {
     console.log(err);
     process.exit(1);
