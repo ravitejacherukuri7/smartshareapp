7ae28109b99eee786dd6469c66f73c5b8eec2976
ravitejacherukuri7 <47173252+ravitejacherukuri7@users.noreply.github.com>
2020-02-28 22:43:49 +0530

    Update helpers.js

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { notify } from 'react-notify-toast';
-export const ROOT_URL = 'https://awesome-shopping-list-api.herokuapp.com/v1';
+export const ROOT_URL = 'https://smartsharereact.herokuapp.com/v1';
 //export const ROOT_URL = 'http://localhost:5000/v1';
 
 export function setAuthorizationToken(token) {
